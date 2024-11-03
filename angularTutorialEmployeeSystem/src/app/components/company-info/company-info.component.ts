6550433fa9d354cdb7f2c9e47ddf881bd7d725f6
Ibitayo <ibitayola@gmail.com>
2024-11-03 18:30:21 +0000

    companies info set up

--- angularTutorialEmployeeSystem/src/app/components/company-info/company-info.component.ts
+++ angularTutorialEmployeeSystem/src/app/components/company-info/company-info.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { CompaniesService } from '../../services/companies.service';
+import { Company } from '../../model/Company';
 
 @Component({
   selector: 'app-company-info',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './company-info.component.html',
-  styleUrl: './company-info.component.scss'
+  styleUrl: './company-info.component.scss',
+  providers: [NgbCarouselConfig]
 })
-export class CompanyInfoComponent {
+export class CompanyInfoComponent implements OnInit {
+  private companiesService: CompaniesService = Inject(CompaniesService);
+  private carouselConfig: NgbCarouselConfig = Inject(NgbCarouselConfig);
+  companiesList: Company[] = [];
 
+  ngOnInit(): void {
+    this.carouselConfig.interval = 3000; // (ms)
+    this.companiesService.getAllCompanies().subscribe({
+      next: (res) => {
+        this.companiesList = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
---
+++ angularTutorialEmployeeSystem/src/app/model/Company.ts
@@ -0,0 +1,6 @@
+export interface Company {
+  id: string;
+  companyName: string;
+  shortDescription: string;
+  imageUrl: string;
+}
