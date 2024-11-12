10c0114720d5617b009effbd3695efa9430d3f20
Ibitayo <ibitayola@gmail.com>
2024-11-12 22:49:00 +0000

    chart.js initial commit

--- angularTutorialEmployeeSystem/src/app/components/dashboard/dashboard.component.ts
+++ angularTutorialEmployeeSystem/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  chart: Chart<'pie', number[], string> | undefined;
 
+  ngOnInit() {
+    this.initializeChart();
+  }
+
+  initializeChart() {
+    this.chart = new Chart('myPie', {
+      type: 'pie',
+      data: this.data,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Pie Chart',
+          },
+        },
+      },
+    });
+  }
+
+  data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)',
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
 }
