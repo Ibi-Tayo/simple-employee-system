2f53c9fb7dc250ad24a6e5e3c8e891fe25da3ea5
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2024-12-11 22:46:40 +0000

    fix get method

--- Server/Data/Employee.cs
+++ Server/Data/Employee.cs
@@ -5,33 +5,33 @@ namespace simple_employee_backend.Data;
 public class Employee
 {
     [JsonPropertyName("id")] 
-    public required string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Id { get; set; } = Guid.NewGuid().ToString();
     
     [JsonPropertyName("name")]
-    public required string Name { get; set; }
+    public string Name { get; set; }
     
     [JsonPropertyName("dateOfBirth")]
-    public required string DateOfBirth { get; set; }
+    public string DateOfBirth { get; set; }
     
     [JsonPropertyName("job")]
-    public required string Job { get; set; }
+    public string Job { get; set; }
     
     [JsonPropertyName("yearsOfExperience")]
     public int YearsOfExperience { get; set; }
     
     [JsonPropertyName("company")]
-    public required string Company { get; set; }
+    public string Company { get; set; }
     
     [JsonPropertyName("city")]
-    public required string City { get; set; }
+    public string City { get; set; }
     
     [JsonPropertyName("salary")]
     public int Salary { get; set; }
     
     [JsonPropertyName("address")]
-    public required string Address { get; set; }
+    public string Address { get; set; }
     
     [JsonPropertyName("postcode")]
-    public required string PostCode { get; set; }
+    public string PostCode { get; set; }
     
 }
